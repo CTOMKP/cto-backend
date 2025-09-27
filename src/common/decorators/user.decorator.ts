173a217996aace338ccbdcfdb810084be335a1cd
emmanuel obaze <obazeemmanuel2@gmail.com>
2025-09-28 08:00:28 +1300

    Complete restructure: Move all backend code to cto-backend folder

--- src/common/decorators/user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-// CurrentUser decorator extracts user from request (JWT strategy attaches it)
-export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
