5ae7e8d4585f6fc78d64deb511581b5b3dd3e3ab
CTO Dev <cto-dev@ctomemes.xyz>
2026-05-09 13:53:05 +0100

    fix(marketplace): import EmailModule for MarketplaceService DI

--- src/marketplace/marketplace.module.ts
+++ src/marketplace/marketplace.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PaymentModule } from '../payment/payment.module';
 import { XpModule } from '../xp/xp.module';
+import { EmailModule } from '../email/email.module';
 import { MarketplaceController } from './marketplace.controller';
 import { MarketplaceService } from './marketplace.service';
 import { MarketplacePricingService } from './marketplace-pricing.service';
 import { MarketplaceCronService } from './marketplace-cron.service';
 
 @Module({
-  imports: [PrismaModule, PaymentModule, XpModule],
+  imports: [PrismaModule, PaymentModule, XpModule, EmailModule],
   controllers: [MarketplaceController],
   providers: [MarketplaceService, MarketplacePricingService, MarketplaceCronService],
   exports: [MarketplaceService, MarketplacePricingService],
